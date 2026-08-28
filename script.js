968e89265ae7d3267d213340a5e60a34d5f74b37
Littleboy40 <42167001+Littleboy40@users.noreply.github.com>
2026-08-28 11:53:48 +0200

    Add button to update grid size proportionally
    The new button allows user to provide a number between 1 and 100 to
    update the grid size and clean up the previous drawing.
    This includes recalculating borders and cell sizes so the overall
    drawing size stays the same. Seems to not work with high grid sizes
    as the border cannot get thin enough.

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 let gridContainer = document.querySelector("#grid-container");
 gridContainer.style.display = "inline-block";
-for (let i = 0; i < 16; i++) {
-    let gridRow = document.createElement("div");
-    gridRow.style.display = "flex";
-    for (let j = 0; j < 16; j++) {
-        let gridCell = document.createElement("div");
-        gridCell.style.border = "thin solid #000000";
-        gridCell.style.padding = "5px";
-        gridRow.appendChild(gridCell);
-    }
-    gridContainer.appendChild(gridRow);
-}
+
+let updateButton = document.querySelector("#user-input");
 
 function highlight(event) {
     event.target.style.backgroundColor = "yellow";
@@ -21,5 +12,38 @@ function unhighlight(event) {
     event.target.style.backgroundColor = "white";
 }
 
+function generateGrid(gridSize) {
+    emptyGrid();
+
+    for (let i = 0; i < gridSize; i++) {
+    let gridRow = document.createElement("div");
+    gridRow.style.display = "flex";
+    for (let j = 0; j < gridSize; j++) {
+        let gridCell = document.createElement("div");
+        gridCell.style.boxSizing = "border-box";
+        gridCell.style.border = `${16/gridSize}px solid #000000`;
+        gridCell.style.padding = `${80/gridSize}px`;
+        gridRow.appendChild(gridCell);
+    }
+    gridContainer.appendChild(gridRow);
+    }
+}
+
+function emptyGrid() {
+    gridContainer.innerHTML = '';
+}
+
+function updateGrid() {
+    let number = prompt("What should the new grid size be? Please enter a number between 1 and 100.");
+    while (isNaN(number) || number < 0 || 100 < number) {
+        number = prompt("Try again!");
+    }
+    generateGrid(number);
+}
+
+let defaultgridSize = 16;
+generateGrid(defaultgridSize);
+
 gridContainer.addEventListener("mouseover", highlight);
-gridContainer.addEventListener("mouseout", unhighlight);
+// gridContainer.addEventListener("mouseout", unhighlight);
+updateButton.addEventListener("click", updateGrid);
